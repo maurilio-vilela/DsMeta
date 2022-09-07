07c936145a03251aea9b333da10bb11576344975
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-09-07 17:57:54 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return(
+      <>
+        <header>
+            <div className="csmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por 
+                    <a href="http://www.vilelatech.com.br" target="_blank"> VilelaTech</a>
+                </p>
+            </div>
+        </header>
+      </>
+    );
+  }
+  
+  export default Header
